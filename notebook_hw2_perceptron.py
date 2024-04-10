4d3877fe4a3ba9061b6ab0c3b631cab6507b9c74
sfmig <33267254+sfmig@users.noreply.github.com>
2024-04-10 16:58:27 +0100

    normalise w and x in w_update_cross

--- notebook_hw2_perceptron.py
+++ notebook_hw2_perceptron.py
@@ -10,12 +10,14 @@ import matplotlib.pyplot as plt
 dim = 2  # for 2D plots I need dim=2
 n_train_samples = 100
 
-centre_positive_samples = np.array([6.0, 2.0]).reshape(-1, 2)
-centre_negative_samples = np.array([2.0, 6.0]).reshape(-1, 2)
+centre_positive_samples = np.array([2.0, 6.0]).reshape(-1, 2)
+centre_negative_samples = np.array([6.0, 2.0]).reshape(-1, 2)
 
+
+epsilon = 0.1
 # %%
 # Define training dataset
-n_positive_samples = np.random.randint(0, n_train_samples)
+n_positive_samples = np.random.randint(1, n_train_samples)
 n_negative_samples = n_train_samples - n_positive_samples
 
 
@@ -64,24 +66,15 @@ def plot_train_set_and_w(w_unit=None, n_iterations=None, fig=None, ax=None):
         # hyperplane: points it goes thru
         ax.axline((0.0, 0.0), (w_unit[1], -w_unit[0]), color="r")
 
-        # if n_iterations:
-        #     scale = 1.25
-        #     alignment = ['left', 'right']
-        #     ax.text(
-        #         scale * w_unit[0],
-        #         scale * w_unit[1],
-        #         f'n={n_iterations}',
-        #         ha=alignment[(0)**(n_iterations % 2)]
-        #     )
-
     # title
     if n_iterations:
         ax.set_title(f"Iteration: {n_iterations}")
 
     # axes
+    
+    # ax.set_xlim(-5, None)
+    # ax.set_ylim(-5, None)
     ax.axis("equal")
-    ax.set_xlim(-3, None)
-    ax.set_ylim(-3, None)
 
     return fig, ax
 
@@ -109,6 +102,15 @@ def w_update_naive(w, x):
 
 
 def w_update_cross(w, x):
+
+    if np.all(w == np.zeros_like(w)):
+        w += epsilon
+
+
+    # unit vectors
+    w = w / np.linalg.norm(w)
+    x = x / np.linalg.norm(x)
+
     # compute normal to the plane pi, defined by w and x
     n = np.cross(
         np.pad(w, (0, 1)),
@@ -122,18 +124,18 @@ def w_update_cross(w, x):
     # Compute vector to add to w, st the resulting normal is w_perp
     d = w_perp[:2] - w
 
-    return w + 2*d
+    return w + 1.1*d
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Implement naive perceptron
 
 # initialise w
-w = np.zeros_like(train_dataset[0, :2]) + 0.1
+w = np.zeros_like(train_dataset[0, :2]) # modified to non-zero if using w_update_cross
 fig, ax = plot_train_set_and_w(w)
 
-n_iterations = 0
-while n_iterations < 50:  #True:
+n_iterations = 0 # n of weight updates
+while n_iterations < 50:  #
     n_misclassif = 0
 
     for x in train_dataset:
@@ -143,7 +145,7 @@ while n_iterations < 50:  #True:
         else:
             # Update rule
             # w += x[:2] * x[2]
-            w = w_update_cross(w, x)
+            w = w_update_cross(w, x)  # w_update_naive(w, x) #
 
             # counters
             n_misclassif += 1
@@ -170,10 +172,5 @@ plt.plot(proj_x_on_w, '.-')
 plt.hlines(0,xmin=0, xmax=proj_x_on_w.shape[0])
 plt.title('Projection of x on w')
 
-# for x in train_dataset
-# plt.scatter()
 
-# %%
-np.sign(w.reshape(1,2)@train_dataset[:,:2].T).T
-np.dot(w, train_dataset[:,:2]) * train_dataset[2]
 # %%
